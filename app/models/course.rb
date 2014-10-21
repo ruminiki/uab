f942c2b5b0480b740130f0792ad04a49389c0b89
Ruminiki Schmoeller <ruminikis@gmail.com>
2014-10-21 21:54:12 -0200

    Converte os atributos para upper case antes de salvar.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,12 @@
 class Course < ActiveRecord::Base
+	validates :name, :uniqueness => { :case_sensitive => false }
+	validates :acronym, :uniqueness => { :case_sensitive => false }
+
+	before_save :upper_case
+
+	def upper_case
+		self.name.upcase!
+		self.acronym.upcase!
+	end
+
 end
