c13053460091382203bc9e9fe55c2094ac13bdb7
Ruminiki Schmoeller <ruminiki.schmoeller@unila.edu.br>
2014-12-15 10:25:05 -0200

    Definido valor default para os campos admin, super e active na tabela de usuários

--- db/migrate/20141018114110_devise_create_users.rb
+++ db/migrate/20141018114110_devise_create_users.rb
@@ -20,9 +20,9 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string   :current_sign_in_ip
       t.string   :last_sign_in_ip
 
-      t.boolean :admin
-      t.boolean :active
-      t.boolean :super
+      t.boolean :admin :super, null: false, default: false
+      t.boolean :active :super, null: false, default: true
+      t.boolean :super, null: false, default: false
       ## Confirmable
       # t.string   :confirmation_token
       # t.datetime :confirmed_at
